38dfdf56757dcfd74da8fec410f6f66e381a5399
Maxyee <eyaminkhan00@gmail.com>
2018-05-18 17:56:24 +0600

    commited the schedule menu api for searching data with page number and mealid

--- CookantsProject/ApiLogic/PaginationApi.cs
+++ CookantsProject/ApiLogic/PaginationApi.cs
@@ -36,3 +36,23 @@
             return new List<object>(items);
         }
         /*Api method for pagination of schedlemenu with date ends here*/
+
+         /*Api Method for pagination of schedluemenu with pageno and mealid starts here*/
+        public List<object> SchedleMenuPagenoMealid(int pageno, int mealid)
+        {
+          
+            int pagevaluesize = 10;
+
+            var menuScheduleAlldata = _menuScheduleRepository.GetAll().ToList();
+            int count = menuScheduleAlldata.Count();
+
+            int totalpage = (int)Math.Ceiling(count / (double)pagevaluesize);
+            var items = menuScheduleAlldata.Skip((pageno - 1) * pagevaluesize).Take(pagevaluesize).Where(x => x.MealId == mealid).ToList();
+
+
+            var previousPage = pageno > 1 ? "Yes" : "No";
+            var nextPage = pageno < totalpage ? "Yes" : "No";
+
+            return new List<object>(items);
+        }
+        /*Api Method for pagination of schedluemenu with pageno and mealid ends here*/
