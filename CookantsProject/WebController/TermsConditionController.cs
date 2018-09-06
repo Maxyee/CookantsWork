24bf4bc39d5c6ffb2ec9f5a9ae39b2661055fd96
Maxyee <eyaminkhan00@gmail.com>
2018-09-07 00:37:34 +0600

    Added the terms and condition controller

---
+++ CookantsProject/WebController/TermsConditionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CookantsEntity;
+using CookantsEntity.Model;
+using CookantsService.Services;
+
+namespace CoockantsWeb.Controllers
+{
+    public class TermsConditionController : Controller
+    {
+        private readonly ITermsService _termsAndCondition;
+        public TermsConditionController(ITermsService termsAndCondition)
+        {
+
+            _termsAndCondition = termsAndCondition;
+        }
+        // GET: About Us
+        public ActionResult Index()
+        {
+            var terms = _termsAndCondition.GetAll();
+            return View(terms);
+        }
+    }
+}
